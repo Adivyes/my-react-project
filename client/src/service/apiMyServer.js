7bd0b74b58766afedf15fb4c4bc80b1eca6b1fa0
Adivyes <Adivyes@gmail.com>
2021-05-11 14:07:21 +0300

    init

--- client/src/service/apiMyServer.js
+++ client/src/service/apiMyServer.js
@@ -1,5 +1,5 @@
 // const baseApi = "http://localhost:8082/users/"
-const baseApi = process.env.NODE_ENV === 'production'? 'placeHolder': 'http://localhost:8082/users/'
+const baseApi = process.env.NODE_ENV === 'production'? 'https://adiv-users-mern-app.herokuapp.com/': 'http://localhost:8082/users/'
 export async function addUser(user){
     const requestOptions = {
         method: 'POST',
--- server/index.js
+++ server/index.js
@@ -1,27 +1,24 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
-const db = require("./DB");
 const app = express();
+
+const db = require("./DB");
 const usersRouter = require('./routes/users-router')
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8082;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
-
 db.on('error', (err)=>{console.log(err)})
-
 app.listen(PORT, () => {
   console.log(`MERN app is connected to serve on server on port: ${PORT}`);
 });
-
-// app.get("/", (req, res) => {
-//   res.send("Get api is working fine");
-// });
 
 app.use('/users', usersRouter)
 
