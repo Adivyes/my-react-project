5fc9e78837e203f2374ad65eb93d20ba1bdd6041
Adivyes <Adivyes@gmail.com>
2021-05-11 12:31:03 +0300

    init

--- server/index.js
+++ server/index.js
@@ -10,15 +10,24 @@ const PORT = process.env.PORT || 8082;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
 db.on('error', (err)=>{console.log(err)})
+
 app.listen(PORT, () => {
   console.log(`MERN app is connected to serve on server on port: ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Get api is working fine");
-});
+// app.get("/", (req, res) => {
+//   res.send("Get api is working fine");
+// });
 
 app.use('/users', usersRouter)
 
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res)=>{
+      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
